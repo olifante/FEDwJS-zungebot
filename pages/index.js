7ce9a6c451007d7127b089d690a9c8dbf1937293
Tiago Henriques <trinosauro@gmail.com>
2017-06-10 22:44:07 +0200

    Try (and fail) to style child component

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,18 @@ function getPosts() {
   ]
 }
 
+const PostLink = ({ post }) =>
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+  </li>
+
 export default () =>
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      {getPosts().map(post =>
-        <li key={post.id}>
-          <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-            <a>{post.title}</a>
-          </Link>
-        </li>
-      )}
+      {getPosts().map(post => <PostLink key={post.id} post={post} />)}
     </ul>
     <style jsx>{`
       h1, a {
