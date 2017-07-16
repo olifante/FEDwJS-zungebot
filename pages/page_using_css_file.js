e9e5219751135a6b07dfa1a41d0d21a7fa28ef7e
Tiago Henriques <trinosauro@gmail.com>
2017-07-16 17:03:07 +0100

    Manually add bootstrap stylesheets to page using css file

--- pages/page_using_css_file.js
+++ pages/page_using_css_file.js
@@ -5,10 +5,26 @@ const Index = function() {
   return (
     <div>
       <Head>
-        <link rel="stylesheet" href="static/main.css"/>
+        <link rel="stylesheet" href="static/main.css" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"
+        />
       </Head>
       <Layout>
-        <p>This is a page using a the main.css file, available in the special folder "static". Any file placed inside "static" can be served as a static file, and can therefore be used for serving CSS files, which can be added to any page in the usual way: by adding a "link" element pointin to "static/main.css" or similar. Other static assets such as images, fonts or any other kind of document (pdf, word, etc...) can be served this way.</p>
+        <p>
+          This is a page using a the main.css file, available in the special
+          folder "static". Any file placed inside "static" can be served as a
+          static file, and can therefore be used for serving CSS files, which
+          can be added to any page in the usual way: by adding a "link" element
+          pointin to "static/main.css" or similar. Other static assets such as
+          images, fonts or any other kind of document (pdf, word, etc...) can be
+          served this way.
+        </p>
       </Layout>
     </div>
   )
