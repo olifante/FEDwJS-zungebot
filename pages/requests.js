fd1b149432a6298a3d9d464911133a07ef8bfa2a
Tiago Henriques <trinosauro@gmail.com>
2017-07-19 18:41:25 +0100

    Customize request list

--- pages/requests.js
+++ pages/requests.js
@@ -3,27 +3,47 @@ import Link from "next/link"
 import BootstrapHead from "../components/BootstrapHead"
 import get from "../services/get"
 import React from "react"
+import { PageHeader, Media, Image } from "react-bootstrap"
 
 const Requests = function(props) {
   return (
     <div>
       <BootstrapHead title="Requests" />
       <Layout>
-        <h1>Requests</h1>
-        <ul>
-          {props.requests.map(request =>
-            <li key={request.id}>
-              <Link
-                as={`/requests/${request.id}`}
-                href={`/request_details?id=${request.id}`}
-              >
-                <a>
-                  {request.subject}
-                </a>
-              </Link>
-            </li>
+        <PageHeader>
+          Requests <small>what, where and when</small>
+        </PageHeader>
+        <div>
+          {props.requests.map((request, index) =>
+            <Media key={request.id}>
+              <Media.Left>
+                <Image
+                  src={`http://lorempixel.com/300/200/city/${index % 10}`}
+                  alt="300x200"
+                  width={300}
+                  height={200}
+                  key={request.id}
+                  rounded
+                />
+              </Media.Left>
+              <Media.Body>
+                <Media.Heading>
+                  <Link
+                    as={`/requests/${request.id}`}
+                    href={`/request_details?id=${request.id}`}
+                  >
+                    <a>
+                      {request.subject}
+                    </a>
+                  </Link>
+                </Media.Heading>
+                <p>
+                  {request.description}
+                </p>
+              </Media.Body>
+            </Media>
           )}
-        </ul>
+        </div>
         <Link href="/request_create">
           <button className="btn btn-primary">add new request</button>
         </Link>
